ffb87ee981362e78c1af5ac1a2b18eae4539addf
Kunal <therawknight@gmail.com>
2023-11-30 10:49:40 +0400

    code updates

--- 10-Azure-Virtual-Network-4Tier/terraform-manifests/c6-01-vnet-input-variables.tf
+++ 10-Azure-Virtual-Network-4Tier/terraform-manifests/c6-01-vnet-input-variables.tf
@@ -37,7 +37,7 @@ variable "app_subnet_name" {
 variable "app_subnet_address" {
   description = "Virtual Network App Subnet Address Spaces"
   type = list(string)
-  default = ["10.0.11.0/24"]
+  default = ["10.0.2.0/24"]
 }
 
 
@@ -51,7 +51,7 @@ variable "db_subnet_name" {
 variable "db_subnet_address" {
   description = "Virtual Network Database Subnet Address Spaces"
   type = list(string)
-  default = ["10.0.21.0/24"]
+  default = ["10.0.3.0/24"]
 }
 
 
@@ -65,7 +65,7 @@ variable "bastion_subnet_name" {
 variable "bastion_subnet_address" {
   description = "Virtual Network Bastion Subnet Address Spaces"
   type = list(string)
-  default = ["10.0.100.0/24"]
+  default = ["10.0.4.0/24"]
 }
 
 

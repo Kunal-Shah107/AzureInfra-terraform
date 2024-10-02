140c56a5f123b1c0fa71fc66e8f987fff8136432
Kunal <therawknight@gmail.com>
2024-10-02 15:53:29 +0400

    Code updated

--- 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
+++ 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
@@ -1,10 +1,10 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.9.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 2.0" # Optional but recommended in production
+      version = ">= 4.0" # Optional but recommended in production
     }    
   }
 }
@@ -15,7 +15,7 @@ provider "azurerm" {
 }
 
 # Create Resource Group 
-resource "azurerm_resource_group" "my_demo_rg1" {
+resource "azurerm_resource_group" "demo_rg" {
   location = "uaenorth"
-  name = "my-demo-rg"  
+  name = "demo-rg"
 }
